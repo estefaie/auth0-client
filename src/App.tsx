d931b5f138fb01bb1696bc1fb1f7aa1321b723b4
Hemen Estefaie <estefaie@gmail.com>
2019-10-20 22:40:19 +1100

    Add NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import NavBar from './NavBar';
+
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div>
+      <NavBar />
       <p>Work in progress.</p>
     </div>
   );
